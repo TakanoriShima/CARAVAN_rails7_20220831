ba8ecedef7f2ad9befe8bf3ee17a3307e2147b1b
EC2 Default User <ec2-user@ip-172-31-0-137.ap-northeast-1.compute.internal>
2022-08-31 03:22:45 +0000

    Chapter 7 削除機能を作ろう

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -27,6 +27,12 @@ class BlogsController < ApplicationController
     redirect_to blog_path(blog)
   end
   
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path, status: :see_other
+  end
+  
   # ---- 以下を追加してください ---- #
   private
   def blog_params
